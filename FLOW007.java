60b57ff169791bf3d80c6c1f92567924e44db041
joemar25 <joemar.cardino000@gmail.com>
2022-07-01 12:16:55 +0800

    Update

---
+++ FLOW007.java
@@ -0,0 +1,162 @@
+package codechief;
+// We have populated the solutions for the 10 easiest problems for your support.
+
+// Click on the SUBMIT button to make a submission to this problem.
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+class Codechef2 {
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(
+                    new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                    BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        // BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+
+        Reader sc = new Reader();
+
+        int t = sc.nextInt();
+        // int t = Integer.parseInt(bi.readLine());
+
+        while (t-- != 0) {
+
+            int num = sc.nextInt();
+            // int num = Integer.parseInt(bi.readLine());
+            int output = 0;
+            int size = String.valueOf(num).length();
+
+            for (int n = size - 1; n >= 0; n--) {
+
+                int q = (int) (num / Math.pow(10, n));
+                output += q * Math.pow(10, size - n - 1);
+
+                num = num % (int) Math.pow(10, n);
+
+            }
+            System.out.println(output);
+
+        }
+
+        // while(t--!=0){
+        // String num = String.valueOf(sc.nextInt());
+        // StringBuilder sb = new StringBuilder(num);
+        // sb.reverse();
+        // num = sb.toString();
+        // System.out.println(Integer.parseInt(num));
+        // }
+
+    }
+}
---
+++ INTEST.java
@@ -0,0 +1,60 @@
+package codechief;
+// We have populated the solutions for the 10 easiest problems for your support.
+
+// Click on the SUBMIT button to make a submission to this problem.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Remember that the class name should be "Main" and should be "public".
+public class INTEST { // public class Main is the original
+	public static void main(String[] args) {
+		// System.in and System.out are input and output streams, respectively.
+		InputStream inputStream = System.in;
+
+		InputReader in = new InputReader(inputStream);
+
+		int n = in.nextInt();
+		int k = in.nextInt();
+
+		int ans = 0;
+
+		for (int i = 0; i < n; i++) {
+			int x = in.nextInt();
+
+			if (x % k == 0) {
+				ans++;
+			}
+		}
+
+		System.out.println(ans);
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
+}
