6dd29b5bef3653b6048b9ea266c9da96d8818532
joemar25 <joemar.cardino000@gmail.com>
2022-07-05 22:26:36 +0800

    Update

--- PASSORFAIL.java
+++ PASSORFAIL.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
-import java.io.File;
 
 class PASSORFAIL {
+
+	static void exam(int n, int x, int p) {
+		int penalty = x - n;
+		int score = x * 3;
+		int total = penalty + score;
+		if (p <= total)
+			System.out.println("PASS");
+		else
+			System.out.println("FAIL");
+	}
+
 	public static void main(String[] args) throws Exception {
 		Scanner in = new Scanner(System.in);
-		// joemar000 -> https://www.codechef.com/submit/PASSORFAIL
-		//
-
+		int t = in.nextInt();
+		while (t > 0) {
+			int n = in.nextInt();
+			int x = in.nextInt();
+			int p = in.nextInt();
+			exam(n, x, p);
+			t--;
+		}
+		in.close();
 	}
 }
