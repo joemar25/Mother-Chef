b50a2957221accfaa748b3238956578ea9e4b1af
I am a cat <joemar.cardino000@gmail.com>
2023-05-10 22:30:29 +0800

    Add files via upload

---
+++ Easy/TwoSum.java
@@ -0,0 +1,25 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        int n = nums.length;
+        int lo = 0, hi = n - 1;
+        int indices[]  = {0, 1};
+
+        if (n == 2)
+            return indices;
+
+        while (lo < n - 1) {
+            while (hi > lo) {
+                if ((nums[lo] + nums[hi]) == target) {
+                    indices[0] = lo;
+                    indices[1] = hi;
+                    return indices;
+                }
+                hi -= 1;
+            }
+            lo += 1;
+            hi = n - 1;
+        }
+        return indices;
+    }
+}
