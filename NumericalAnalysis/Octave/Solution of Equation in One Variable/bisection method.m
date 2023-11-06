433219a16427f90e14fc2eabb4e199e507043ec8
joemar25 <joemar.cardino000@gmail.com>
2023-11-06 10:40:42 +0800

    Refactor bisection method implementation and add table output

--- NumericalAnalysis/Octave/Solution of Equation in One Variable/bisection method.m
+++ NumericalAnalysis/Octave/Solution of Equation in One Variable/bisection method.m
@@ -6,48 +6,58 @@
 
 % The Function implemented using the Algorithm for Bisection Method for root finding
 function p = bisection_method(f, a, b, TOL, N)
-  % Step 1
-  i = 1;
-  FA = f(a);
 
-  % Step 2
-  while i <= N
-    % Step 3
-    p = a + (b - a) / 2;
-    FP = f(p);
+    #{
+    Note:-10 field width to the format specifiers in the printf function.
+    This means that each output will be left - justified and padded with spaces
+    to a total width of 10 characters.
+    #}
+    printf('%-10s %-10s %-10s %-10s %-10s\n', 'n', 'An', 'Bn', 'Pn', 'f(Pn)')
 
-    % Step 4
-    if FP == 0 || (b - a) / 2 < TOL
-      fprintf('Approximate solution: %f\n', p);
-      return;
+    % Step 1
+    i = 1;
+    FA = f(a);
+
+    % Step 2
+    while i <= N
+        % Step 3
+        p = a + (b - a) / 2;
+        FP = f(p);
+
+        % Step 4
+        if FP == 0 || (b - a) / 2 < TOL
+            printf('%-10d %-10f %-10f %-10f %-10f\n', i, a, b, p, FP);
+            % return; % Note: uncomment to only get the first iteration
+        else
+            printf('%-10s %-10s %-10s %-10s %-10s\n', '-', '-', '-', '-', '-');
+        end
+
+        % Step 5
+        i = i + 1;
+
+        % Step 6
+        if FA * FP > 0
+            a = p;
+            FA = FP;
+        else
+            b = p;
+        end
+
     end
 
-    % Step 5
-    i = i + 1;
-
-    % Step 6
-    if FA * FP > 0
-      a = p;
-      FA = FP;
-    else
-      b = p;
-    end
-  end
-
-  % Step 7
-  fprintf('Method failed after %d iterations\n', N_0);
-  fprintf('The procedure was unsuccessful.\n');
+    % Step 7
+    % printf('Method failed after %d iterations\n', N);
+    % printf('The procedure was unsuccessful.\n');
 end
 
 % clear screen
 clc, clear
 
-% Main Area for Input and Displaying of Output
-f = @(x) x^3 + 4*x^2 - 10;  % equation
-TOL = 5;                    % tolerance
-N = 10;                     % max num of iterations
+% Main Area for Input and Displaying of Output(Initial)
+f = @(x) x^3 + 4 * x^2 - 10; % equation
+TOL = 1; % tolerance 1e-4 or 10 ^ -4
+N = 13; % max num of iterations
 a = 1;
 b = 2;
 
 p = bisection_method(f, a, b, TOL, N);
-
--- NumericalAnalysis/Other/following_bisection_method._algo.py
+++ NumericalAnalysis/Other/following_bisection_method._algo.py
@@ -1,5 +1,3 @@
-from fractions import Fraction
-
 # INPUT endpoints a,b; tolerance TOL; maximum numbers of iterations N.
 # OUTPUT approximate solution p or message of failure.
 
@@ -25,12 +23,15 @@ f = lambda x: ((x) ** 3) + 4 * ((x) ** 2) - 10
 # INPUT endpoints a,b; tolerance TOL; maximum numbers of iterations n_0.
 a = 1
 b = 2
-TOL = 5
-N = 10
+TOL = 1
+N = 13
 
 # Step 1 Set i = 1
 i = 1
 FA = f(a)
+
+# Print the header for the table
+print("{:<10s} {:<10s} {:<10s} {:<10s} {:<10s}".format("n", "An", "Bn", "Pn", "f(Pn)"))
 
 # Step 2 While i <= N do Steps 3-6.
 while i <= N:
@@ -40,8 +41,8 @@ while i <= N:
 
     # Step 4 If FP = 0 or (b - a) / 2 < TOL then
     if FP == 0 or (b - a) / 2 < TOL:
-        print(f"Approximate solution: {p}")  # Procedure completed successfully.
-        break
+        print("{:<10d} {:<10f} {:<10f} {:<10f} {:<10f}".format(i, a, b, p, FP))
+        # break
 
     # Step 5 Set i = i + 1.
     i += 1
@@ -54,6 +55,6 @@ while i <= N:
         b = p  # FA is unchanged.
 
 # Step 7 Output ('Method failed after N iterations, N, =',N_o);
-else:
-    print(f"Method failed after N iterations, N = {i}")
-    print("The procedure was unsuccessful.")
+# else:
+# print(f"Method failed after N iterations, N = {i}")
+# print("The procedure was unsuccessful.")
